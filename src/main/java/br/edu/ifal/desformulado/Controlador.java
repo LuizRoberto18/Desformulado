dbf5721678f8832474a6330ffb398a55089d05b6
Luiz Roberto <passageiro1829@gmail.com>
2019-11-20 19:29:41 -0300

    a luta e grande mas a derrota e certa

--- src/main/java/br/edu/ifal/desformulado/Controlador.java
+++ src/main/java/br/edu/ifal/desformulado/Controlador.java
@@ -3,6 +3,7 @@ package br.edu.ifal.desformulado;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class Controlador{
@@ -21,6 +22,26 @@ public class Controlador{
         retorno.addObject("aluno", a);
         return retorno ;
     }
-
-
+    
+    @RequestMapping("/lista")
+    public ModelAndView lista() {
+    	return new ModelAndView("Lista.html");
+    }
+    	
+    @RequestMapping("/cadastro")
+    public ModelAndView cadastrar(Aluno aluno, RedirectAttributes redirect) {
+    	alunoRepo.save(aluno);
+    	ModelAndView retorno = new ModelAndView("redirect:/Lista.html");
+    	redirect.addFlashAttribute("mensagem", aluno.getNome() + "cadastrado com sucesso");
+    	return retorno;
+    }
+    
+    @RequestMapping("/listar_alunos")
+	public ModelAndView listarAlunos(){
+		ModelAndView retorno = new ModelAndView("Lista.html");
+		Iterable<Aluno> alunos = alunoRepo.findAll();
+		retorno.addObject("alunos", alunos);
+		return retorno;
+	}
+    
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO ALUNO(ID,EMAIL, NOME,SENHA, MODULO) 
+VALUES(25,'bruno@gmail.com','Bruno', '12345', 'Segundo');
